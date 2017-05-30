03e5ee0927bfa1f388a9089f9e6e676cde57301e
Anton Oparin <mavarazy@gmail.com>
2017-05-30 19:24:16 +0300

    #10 Adding failing test TDD, so exciting :)

--- test/com/clemble/loveit/payment/controller/ThankTransactionControllerSpec.scala
+++ test/com/clemble/loveit/payment/controller/ThankTransactionControllerSpec.scala
@@ -26,4 +26,6 @@ class ThankTransactionControllerSpec extends ControllerSpec {
 
   }
 
+
+
 }
--- test/com/clemble/loveit/thank/service/ThankServiceSpec.scala
+++ test/com/clemble/loveit/thank/service/ThankServiceSpec.scala
@@ -1,9 +1,10 @@
 package com.clemble.loveit.thank.service
 
 import com.clemble.loveit.common.ServiceSpec
-import com.clemble.loveit.common.model.HttpResource
+import com.clemble.loveit.common.model.{Amount, HttpResource, Resource, UserID}
 import com.clemble.loveit.test.util.UserGenerator
 import com.clemble.loveit.thank.model.ResourceOwnership
+import com.clemble.loveit.user.model.User
 import com.clemble.loveit.user.service.repository.UserRepository
 import org.apache.commons.lang3.RandomStringUtils._
 import org.junit.runner.RunWith
@@ -16,32 +17,60 @@ class ThankServiceSpec(implicit val ee: ExecutionEnv) extends ServiceSpec {
   val thankService = dependency[ThankService]
   val userRepo = dependency[UserRepository]
 
+  def createScene():(Resource, User, User) = {
+    val url = HttpResource(s"example.com/some/${randomNumeric(10)}")
+    val owner = await(userRepo.save(UserGenerator.generate().assignOwnership(0, ResourceOwnership.full(url))))
+    val giver = await(userRepo.save(UserGenerator.generate()))
+
+    (url, owner, giver)
+  }
+
+  def thank(user: UserID, url: Resource) = {
+    await(thankService.thank(user, url))
+  }
+
+  def getBalance(user: String): Amount = {
+    await(userRepo.findById(user).map(_.get)).balance
+  }
+
+  def getBalance(url: Resource): Amount = {
+    await(thankService.getOrCreate(url)).given
+  }
+
   "Thank " should {
 
+
     "Decrement for the giver" in {
-      val url = HttpResource(s"example.com/some/${randomNumeric(10)}")
+      val (url, _, giver) = createScene()
 
-      val giver = UserGenerator.generate()
+      thank(giver.id, url)
+      getBalance(url) shouldEqual 1
 
-      val beforeThank = await(userRepo.save(giver))
-      val thank = await(thankService.thank(giver.id, url))
-      val afterThank = await(userRepo.findById(giver.id).map(_.get))
-      thank.given shouldEqual 1
-      beforeThank.balance - 1 shouldEqual afterThank.balance
+      val giverBalanceAfterThank = getBalance(giver.id)
+      giver.balance - 1 shouldEqual giverBalanceAfterThank
     }
 
     "Increment for the owner" in {
-      val url = HttpResource(s"example.com/some/${randomNumeric(10)}")
-      val owner = UserGenerator.generate().assignOwnership(0, ResourceOwnership.full(url))
-      val giver = UserGenerator.generate()
+      val (url, owner, giver) = createScene()
 
-      await(userRepo.save(giver))
-      val beforeThank = await(userRepo.save(owner))
-      val thank = await(thankService.thank(giver.id, url))
-      val afterThank = await(userRepo.findById(owner.id).map(_.get))
+      thank(giver.id, url)
+      getBalance(url) shouldEqual 1
 
-      thank.given shouldEqual 1
-      (beforeThank.balance + 1) shouldEqual afterThank.balance
+      val ownerBalanceAfterThank = getBalance(owner.id)
+      (owner.balance + 1) shouldEqual ownerBalanceAfterThank
+    }
+
+    "Double thank neutralizes effect" in {
+      val (url, owner, giver) = createScene()
+
+      // Double thank neutralized each other
+      thank(giver.id, url)
+      thank(giver.id, url)
+      getBalance(url) shouldEqual 0
+
+      // Balance did not change
+      getBalance(owner.id) shouldEqual owner.balance
+      getBalance(giver.id) shouldEqual giver.balance
     }
 
   }

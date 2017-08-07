b8590cea0485127a384326d1519dc7acabfa6d50
Anton Oparin <mavarazy@gmail.com>
2017-08-07 14:53:50 +0200

    Updating transactions fetching

--- app/com/clemble/loveit/payment/controller/ThankTransactionController.scala
+++ app/com/clemble/loveit/payment/controller/ThankTransactionController.scala
@@ -1,9 +1,14 @@
 package com.clemble.loveit.payment.controller
 
 import com.clemble.loveit.payment.service.ThankTransactionService
-import com.clemble.loveit.common.util.{AuthEnv}
+import com.clemble.loveit.common.util.AuthEnv
 import javax.inject.{Inject, Singleton}
+
+import akka.stream.Materializer
+import akka.stream.scaladsl.Sink
+import com.clemble.loveit.payment.model.ThankTransaction
 import com.mohiva.play.silhouette.api.Silhouette
+import play.api.libs.json.Json
 import play.api.mvc.Controller
 
 import scala.concurrent.ExecutionContext
@@ -12,12 +17,15 @@ import scala.concurrent.ExecutionContext
 case class ThankTransactionController @Inject()(
                                                  transactionService: ThankTransactionService,
                                                  silhouette: Silhouette[AuthEnv],
+                                                 implicit val m: Materializer,
                                                  implicit val ec: ExecutionContext
 ) extends Controller {
 
-  def listMy() = silhouette.SecuredAction(implicit req => {
+  def listMy() = silhouette.SecuredAction.async(implicit req => {
     val thanks = transactionService.list(req.identity.id)
-    Ok.chunked(thanks)
+    thanks.
+      runWith(Sink.seq[ThankTransaction]).
+      map(tr => Ok(Json.toJson(tr)))
   })
 
 }

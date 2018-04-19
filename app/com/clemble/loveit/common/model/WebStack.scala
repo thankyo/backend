9a6d4150fe17746da61b041b6405842fe495b906
Anton Oparin <mavarazy@gmail.com>
2018-04-19 17:54:37 +0700

    fix #71

--- app/com/clemble/loveit/common/model/WebStack.scala
+++ app/com/clemble/loveit/common/model/WebStack.scala
@@ -8,12 +8,14 @@ sealed trait WebStack
 
 case object WordPress extends WebStack
 case object Tumblr extends WebStack
+case object LandingLion extends WebStack
 
 object WebStack {
 
   implicit val jsonFormat: Format[WebStack] = new Format[WebStack] {
     val WORD_PRESS = JsString("WordPress")
     val TUMBLR = JsString("Tumblr")
+    val LANDING_LION = JsString("LandingLion")
 
     override def writes(o: WebStack): JsValue = o match {
       case WordPress => WORD_PRESS
@@ -23,6 +25,7 @@ object WebStack {
     override def reads(json: JsValue): JsResult[WebStack] = json match {
       case WORD_PRESS => JsSuccess(WordPress)
       case TUMBLR => JsSuccess(Tumblr)
+      case LANDING_LION => JsSuccess(LandingLion)
       case _ => JsError("No WebStack associated")
     }
   }
